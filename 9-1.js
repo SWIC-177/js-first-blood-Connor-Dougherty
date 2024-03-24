a549b66108a37e5089cd156247dc413ce794ee4a
Connor-Dougherty <156982019+Connor-Dougherty@users.noreply.github.com>
2024-03-23 21:52:09 -0500

    Update and rename 9-1.html to 9-1.js

---
+++ 9-1.js
@@ -0,0 +1,13 @@
+const name = Connor;
+const age = 18;
+
+function verifyAdulthood (x,y) {
+
+    if  (x >= 18) return console.log(`Welcome, ${y}!`;
+    
+    return console.log(`You are not old enough to enter, ${y}.`);
+}
+
+const screenOutput = verifyAdulthood(age,name);
+
+console.log(screenoutput);
