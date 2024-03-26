6cff39e8b9820c96e3464031bc5dd31c3c9b276d
Manav Misra <manavm@visionwebsoft.com>
2024-03-25 20:29:55 -0500

    :fire: Remove `console.log` on the `return`

--- 9-1.js
+++ 9-1.js
@@ -2,10 +2,8 @@ const name = Connor;
 const age = 18;
 
 function verifyAdulthood (x,y) {
-
     if  (x >= 18) return console.log(`Welcome, ${y}!`;
-    
-    return console.log(`You are not old enough to enter, ${y}.`);
+    return `You are not old enough to enter, ${y}.`;
 }
 
 const screenOutput = verifyAdulthood(age,name);
