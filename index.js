7decc723ec524ec22a0a6aab2bdc6d01d2cb8963
Connor Dougherty <connor.dougherty@student.swic.edu>
2024-03-28 22:38:22 -0500

    Did part 1 of 10-2

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function greet(name) {
-  console.log("Hello, " + name + "!");
+//function greet(name) {
+//  console.log("Hello, " + name + "!");
+//}
+
+//greet("Connor");
+
+
+const person = {
+  name: "John",
+  age: 17,
+};
+
+function verifyAdulthood(personName, personAge) {
+  if (personAge >= 18) return `Welcome, ${personName}!`;
+
+  return `You are not old enough to enter, ${personName}.`;
 }
 
-greet("Connor");
+const adultMessage = verifyAdulthood(personName, personAge);
+
+console.log(adultMessage);
