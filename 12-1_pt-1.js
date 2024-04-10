8fb0592e5dcc30e6226ed689bc6f2a3529ef4d1f
Connor Dougherty <connor.dougherty@student.swic.edu>
2024-04-10 00:18:51 -0500

    12-tuesday homework

---
+++ 12-1_pt-1.js
@@ -0,0 +1,19 @@
+const person = {
+    name: "John Doe",
+    age: 23,
+    address: {
+      street: "123 Main St",
+      city: "New York",
+      state: "NY",
+      zip: "10001",
+    },
+  };
+  
+  
+  const otherPerson = { ...person, hobbies: ["drawing"]};
+  
+  otherPerson.name = "Jane Doe";
+  otherPerson.age = "26";
+  
+  console.log(`Person's name is: ${person.name}`);
+  console.log(`Other Person's name is: ${otherPerson.name}`);
